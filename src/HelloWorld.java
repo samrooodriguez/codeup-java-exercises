b986af47d5b56210318f99272a7671fcf7e8c862
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-01-07 14:37:37 -0600

    finished the exercise for Syntax Types and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,19 +1,22 @@
 public class HelloWorld {
    public static void main(String[] args){
-       System.out.print("Hello,");
-       System.out.print(" World!");
+       System.out.println("Hello,");
+       System.out.println(" World!");
 
        int myFavoriteNumber = 2;
        System.out.println(myFavoriteNumber);
 
+       String myString = "Sam Rodriguez";
+       System.out.println(myString);
+
+       float myNumber = 3.14F;
+       System.out.println(myNumber);
 
 
-
-
-
-
-
-
+       int x = 3;
+       int y = 4;
+       y = y * x;
+       System.out.println(y);
 
    }
 }
