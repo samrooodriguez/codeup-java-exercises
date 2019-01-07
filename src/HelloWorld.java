315e9eb120e59b754fac5311235b6e58a2ede07d
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-01-07 13:51:46 -0600

    number one of the exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,21 @@ public class HelloWorld {
    public static void main(String[] args){
        System.out.print("Hello,");
        System.out.print(" World!");
+
+       int myFavoriteNumber = 2;
+       System.out.println(myFavoriteNumber);
+
+
+
+
+
+
+
+
+
+
    }
 }
+
 
 
