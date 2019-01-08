4b3fdface0a1315cbd73e157e4d5dffe27f6f291
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-01-08 12:26:00 -0600

    finished the third part of the exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -6,10 +6,40 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 
-        int x = 100;
-        do {
-            System.out.print(x+ "\n");
-            x -= 5;
-        } while (x >= -10);
+//        int x = 2;
+//        do {
+//            System.out.print(x+ "\n");
+//            x += 2;
+//        } while (x <= 100);
+
+//      int x = 100;
+//        do {
+//            System.out.print(x+ "\n");
+//            x -= 5;
+//        } while (x >= -10);
+
+//        long a = 2;
+//        do{
+//            System.out.print(a + "\n");
+//            a *= a;
+//        }while(a < 1000000);
+
+//        for(int i = 5; i <= 15; i++){
+//            System.out.print(i + "\n");
+//        }
+
+//        for(int x = 2; x <= 100;x+=2){
+//            System.out.print(x + "\n");
+//        }
+
+//        for(int x = 100; x >= -10;x -= 5){
+//            System.out.print( x + "\n");
+//        }
+
+//        for(long a = 2; a < 1000000; a*=a){
+//            System.out.print(a + "\n");
+//        }
+
+
     }
 }
