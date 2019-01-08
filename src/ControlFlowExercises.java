d713de5c79f2a729881fd5aa185973637ceb13dd
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-01-08 12:08:22 -0600

    did portion b of the exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
-        int i = 5;
-        while(i <= 15 ){
-            System.out.print(i + "\n");
-            i++;
-        }
+//        int i = 5;
+//        while(i <= 15 ){
+//            System.out.print(i + "\n");
+//            i++;
+//        }
 
-
+        int x = 100;
+        do {
+            System.out.print(x+ "\n");
+            x -= 5;
+        } while (x >= -10);
     }
 }
