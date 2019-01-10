998f4a5f16e073aaca854d6ae0abde9962935270
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-01-10 15:55:55 -0600

    thursday exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,31 +1,74 @@
+import java.util.Scanner;
+import java.util.Random;
 public class MethodsExercises {
-
-    public static int addition(int numberA, int numberB){
+    public static int addition(int numberA, int numberB) {
         return numberA + numberB;
     }
-    public static int subtraction(int numberA, int numberB){
+    public static int subtraction(int numberA, int numberB) {
         return numberA - numberB;
     }
-    public static int multiplication(int a, int b){
+    public static int multiplication(int a, int b) {
         int total = 0;
-
-        for(int x = 0; x < a; x++){
+        for (int x = 0; x < a; x++) {
             total += b;
         }
         return total;
     }
-    public static int division(int numberA, int numberB){
+    public static int division(int numberA, int numberB) {
         return numberA / numberB;
     }
-    public static int modulus(int numberA, int numberB){
+    public static int modulus(int numberA, int numberB) {
         return numberA % numberB;
     }
-    public static void main(String[] args){
-        System.out.println(addition(5,5));
-        System.out.println(subtraction(5,4));
-        System.out.println(multiplication(2,8));
-        System.out.println(division(10,2));
-        System.out.println(modulus(4,8));
+    public static int getInteger(int min, int max){
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        System.out.format("Enter in a number between %s and %s: ", min, max);
+        int input = scanner.nextInt();
+
+        if(input < min){
+            System.out.format(input + " is less than %s value %n", min);
+        }
+        else if(input > min && input < max){
+            System.out.println(input + " is within the desired range");
+            return input;
+        }
+        System.out.println("Please enter a valid number");
+        return getInteger(min,max);
+    }
+
+    public static void factorial(){
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Enter an integer from 1 to 10:");
+        int x = scanner.nextInt();
+        String output = "";
+        for(int i = 1; i <= x; i++){
+            output += i + " x ";
+            System.out.format("%d! = %s %n",i,output);
+        }
+    }
+    public static void diceRoll(){
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        Random random = new Random();
+        System.out.println("Please enter the number for a pair of dice:");
+        int userInput = scanner.nextInt();
+        double diceOne = Math.floor(Math.random() * 49 + 1);
+        double diceTwo = Math.floor(Math.random() * 49 + 1);
+
+        System.out.println("Dice one:" + diceOne);
+        System.out.println("Dice Two:" + diceTwo);
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(addition(5, 5));
+        System.out.println(subtraction(5, 4));
+        System.out.println(multiplication(2, 8));
+        System.out.println(division(10, 2));
+        System.out.println(modulus(4, 8));
+        getInteger(1,10);
+        factorial();
+        diceRoll();
 
     }
 }
---
+++ src/MethodsPractice.java
@@ -0,0 +1,123 @@
+// Write a method named countOdds(start, end) that returns an integer containing the integer that is the count of all odd numbers between the start and the end input integers.
+//
+// Write a method named countEvens(start, end) that returns an integer containing the integer that is the count of all even numbers between the start and the end input integers.
+//
+// Write a method named isVowel() that accepts a String input of length 1 and returns a boolean if that string is a vowel other than "y".
+//
+// Write a method named hasVowels() that accepts a string of any length and returns a boolean if there are any vowels in that string.
+//
+// Write a method named countVowels() that accepts a string of any length and returns an integer count of the number of vowels in the provided input String.
+//
+// Write a solution to FizzBuzz using recursion instead of a loop.
+//
+// Write a method named isPrime() that that accepts in an integer number and returns a boolean if the number is evenly divisible only by either 1 or the number itself.
+//
+//Write a method named getTwentyPrimes() that returns a string containing the first 20 prime numbers, each separated by a comma. Output:
+// "1, 2, 3, 5, 7, 11, 13, 17, 19"... until we have a total count of 20 primes in the string.
+
+import java.util.Scanner;
+
+public class MethodsPractice {
+    public static char firstChar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter in any string to get the first character:");
+        String userInput = scanner.next();
+        char first = userInput.charAt(0);
+        System.out.println("The first character in your string is: " + first);
+        return first;
+    }
+
+    public static char secondChar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter in any string to get the second character:");
+        String userInput = scanner.next();
+        char second = userInput.charAt(1);
+        System.out.println("The second character in your string is: " + second);
+        return second;
+    }
+
+    public static char thirdChar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter in any string to get the third character:");
+        String userInput = scanner.next();
+        char third = userInput.charAt(2);
+        System.out.println("The third character in your string is: " + third);
+        return third;
+    }
+
+    public static char lastChar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter in any string to get the last character");
+        String userInput = scanner.next();
+        char last = userInput.charAt(userInput.length() - 1);
+        System.out.println("The last character in your string is: " + last);
+        return last;
+    }
+
+    public static char secondToLastChar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter in any string to get the second to last character");
+        String userInput = scanner.next();
+        char secondToLast = userInput.charAt(userInput.length() - 2);
+        System.out.println("The last character in your string is: " + secondToLast);
+        return secondToLast;
+    }
+
+    public static boolean userWantsToContinue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to continue?");
+        String userInput = scanner.next();
+        boolean cont;
+        if (userInput.equalsIgnoreCase("Yes") || userInput.equals("y")) {
+            cont = true;
+            System.out.println("You chose to continue");
+        } else {
+            cont = false;
+            System.out.println("You did not choose to continue");
+        }
+        return cont;
+    }
+
+    public static boolean isEven(int i) {
+        boolean bool;
+        if (i % 2 == 0) {
+            bool = true;
+        } else {
+            bool = false;
+        }
+        return bool;
+    }
+
+    public static boolean isOdd(int i) {
+        boolean bool;
+        if (i % 2 == 1) {
+            bool = true;
+        } else {
+            bool = false;
+        }
+        return bool;
+    }
+    public static int countOdds(int start, int end) {
+        int output = 0;
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) {
+                output += 1;
+            }
+        }
+        System.out.format("%d %n", output);
+        return output;
+    }
+    public static void main(String[] args) {
+//            firstChar();
+//            secondChar();
+//            thirdChar();
+//            lastChar();
+//            secondToLastChar();
+//            userWantsToContinue();
+//            isEven(10);
+//            isOdd(9);
+//            countOdds(1, 10);
+    }
+}
+
+
