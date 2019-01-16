d4291c5c4456682af2fbc6cd6a05694faec2301d
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-01-16 09:07:20 -0600

    finihsed the server name generator

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class ServerNameGenerator {
+    private static String[] nouns = {
+            "Architecture",
+            "Carnation",
+            "Distance",
+            "Fill",
+            "Glasses",
+            "Saviour",
+            "Retina",
+            "Wrist",
+            "Sphinx",
+            "Dog"
+    };
+    private static String[] adjectives = {
+            "unable",
+            "loving",
+            "secretive",
+            "five",
+            "ruddy",
+            "delicious",
+            "selective",
+            "utter",
+            "neat",
+            "tasty"
+    };
+
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        System.out.println(
+        adjectives[rnd.nextInt(adjectives.length - 1)]
+        + " " +
+        nouns[rnd.nextInt(nouns.length - 1)]);
+    }
+
+}
